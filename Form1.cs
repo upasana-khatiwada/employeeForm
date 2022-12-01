3ce9de7a75ff8fb4729d6fa957bb2b9c6f3600fa
upasana-khatiwada <upashanakhatiwada1234@gmail.com>
2022-12-01 09:32:35 +0545

    "insert data ino database"

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Data.SqlClient;
 
 namespace employeeform
@@ -12,15 +13,7 @@ namespace employeeform
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-           
-        }
-
-
-    
+        }    
         private void name1_TextChanged(object sender, EventArgs e)
         {
             string e_name = name1.Text;
@@ -44,26 +37,64 @@ namespace employeeform
         private void button1_Click(object sender, EventArgs e)
         {
             con.Open();
+
+
+            //--------------------inserting data into database--------------------(method 1)
+
+            //string query = "Insert into Employee " +
+            //               " (e_name,e_address,e_age,e_contact)" +
+            //               "values (' " +
+            //                name1.Text +
+            //               "','" + address1.Text +
+            //               "','" + age1.Text +
+            //               "','" + contact1.Text +
+            //               "')";
+
+            //SqlCommand cmd = con.CreateCommand();
+            //cmd.CommandText = query;
+            //cmd.ExecuteNonQuery();
+            //con.Close();
+
+
+            //--------------------inserting data into database--------------------(method 2)
             string query = "Insert into Employee " +
-               " (e_name,e_address,e_age,e_contact)" +
-               "values (' " +
-               name1.Text +
-               "','" + address1.Text +
-               "','" + age1.Text +
-               "','" + contact1.Text +
-               "')";
+                           " (e_name,e_address,e_age,e_contact)" +
+                           "values (@employee_name,@address,@age,@contact )";
 
             SqlCommand cmd = con.CreateCommand();   
             cmd.CommandText = query;
+            cmd.Parameters.AddWithValue("@employee_name",name1.Text);
+            cmd.Parameters.AddWithValue("@address", address1.Text);
+            cmd.Parameters.AddWithValue("@age", age1.Text);
+            cmd.Parameters.AddWithValue("@contact", contact1.Text);
             cmd.ExecuteNonQuery();
             con.Close();
 
-            String[] row = new String[5];
-            row[0] = name1.Text.ToString();
-            row[1] = address1.Text.ToString();
-            row[2] = age1.Text.ToString();
-            row[3] = contact1.Text.ToString();
-            dataGridView1.Rows.Add(row);
+
+            //---------------displaying  data into gridview-----------------------------
+            //String[] row = new String[5];
+            //row[0] = name1.Text.ToString();
+            //row[1] = address1.Text.ToString();
+            //row[2] = age1.Text.ToString();
+            //row[3] = contact1.Text.ToString();
+            //dataGridView1.Rows.Add(row);
+
+            //----------------------------------------------------------------------
+            string query1 = "Select *from Employee";
+            SqlCommand SqlCommand = new SqlCommand(query1,con);
+            SqlDataAdapter sda = new SqlDataAdapter(SqlCommand);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            int sn = 1;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string employee_name = dt.Rows[i]["name"].ToString();
+                string address = dt.Rows[i]["name"].ToString();
+                string age = dt.Rows[i]["name"].ToString();
+                string contact = dt.Rows[i]["name"].ToString();
+                dataGridView1.Rows.Add(sn++, employee_name, address, age, contact);
+
+            }
         }
     }
 }
